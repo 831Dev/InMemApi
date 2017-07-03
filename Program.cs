24b8973276013f3dbb7737bc73c57d8dc8dd2a5d
eric chavez <echavez@gmail.com>
2017-07-02 23:32:50 -0700

    added command line

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace InMemApi
 {
@@ -12,8 +13,14 @@ namespace InMemApi
     {
         public static void Main(string[] args)
         {
+
+			var config = new ConfigurationBuilder()
+				.AddCommandLine(args)
+				.Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
